60d2f653fd0ef72ac7966bdb1a056e78f49f42b6
uc-ahmednu <77595864+uc-ahmednu@users.noreply.github.com>
2021-01-31 22:35:53 -0500

    Delete sysinfo.ps1.ps1

--- Powershell/sysinfo.ps1.ps1
+++
@@ -1,9 +0,0 @@
-﻿$Hello = "Hello, powershell"
-Write-Host($Hello)
-function getIP {
-(Get-NetIPAddress).IPv4Address | Select-String "192"
-}
-write-host(getIP)
-$IP = getIP
-Write-Host("This machines IP is $IP")
-Write-Host("This machine's IP is (0)" -f $IP)
